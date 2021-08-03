8c8851edfdc83bfa67126c9994d1d7fbb7f8f498
Zishad Hossain Limon <zishadlimon@gmail.com>
2021-08-04 02:41:05 +0600

    repo updated!

---
+++ Assignment2/Inventory-Management-System2/DAL/CartRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CartRepo
+    {
+        static InventoryMS2Entities context;
+        static CartRepo()
+        {
+            context = new InventoryMS2Entities();
+        }
+
+        public static List<CartTable> GetCarts()
+        {
+            return context.CartTables.ToList();
+        }
+
+        public static void AddToCart(CartTable d)
+        {
+            context.CartTables.Add(d);
+            context.SaveChanges();
+        }
+
+        public static void CleanCart()
+        {
+            var cart = GetCarts();
+            foreach (var d in cart)
+            {
+                context.CartTables.Remove(d);
+                context.SaveChanges();
+            }
+        }
+
+        public static CartTable GetCartItem(int id)
+        {
+            return context.CartTables.FirstOrDefault(e => e.Id == id);
+        }
+
+        public static void DeleteCartItem(int id)
+        {
+            var d = GetCartItem(id);
+            context.CartTables.Remove(d);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Assignment2/Inventory-Management-System2/DAL/CategoryRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CategoryRepo
+    {
+        static InventoryMS2Entities context;
+        static CategoryRepo()
+        {
+            context = new InventoryMS2Entities();
+        }
+
+        public static List<string> GetCategoryNames()
+        {
+            var data = context.Categories.Select(e => e.Name).ToList();
+            return data;
+        }
+
+        public static List<Category> GetCategories()
+        {
+            return context.Categories.ToList();
+        }
+
+        public static void AddCategory(Category d)
+        {
+            context.Categories.Add(d);
+            context.SaveChanges();
+        }
+
+        public static Category GetCategoryDetail(int id)
+        {
+            return context.Categories.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
