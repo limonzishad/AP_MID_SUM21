de3df0193ae80998d137e595c9707f40b01dc0a5
Zishad Hossain Limon <zishadlimon@gmail.com>
2021-08-04 02:23:21 +0600

    repo complete!

---
+++ Assignment2/Inventory-Management-System2/BLL/CartService.cs
@@ -0,0 +1,33 @@
+﻿using BEL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CartService
+    {
+        public static List<CartModel> GetCarts()
+        {
+            var orders = CartRepo.GetCarts();
+            var data = AutoMapper.Mapper.Map<List<CartTable>, List<CartModel>>(orders);
+            return data;
+        }
+        public static void AddToCart(ProductModel e)
+        {
+            var pro = new CartModel();
+
+            pro.ProductId = e.Id;
+            pro.ProductName = e.Name;
+            pro.Price = e.Price;
+            pro.Qty = 1;
+            pro.TotalPrice = e.Price;
+
+            var d = AutoMapper.Mapper.Map<CartModel, CartTable>(pro);
+            CartRepo.AddToCart(d);
+        }
+    }
+}
---
+++ Assignment2/Inventory-Management-System2/DAL/OrderRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class OrderRepo
+    {
+        static InventoryMS2Entities context;
+        static OrderRepo()
+        {
+            context = new InventoryMS2Entities();
+        }
+
+        public static List<Order> GetOrders()
+        {
+            return context.Orders.ToList();
+        }
+
+        public static void CreateOrder(Order d)
+        {
+            context.Orders.Add(d);
+            context.SaveChanges();
+        }
+
+        public static void CleanOrder()
+        {
+
+            var cart = GetOrders();
+            foreach (var d in cart)
+            {
+                context.Orders.Remove(d);
+                context.SaveChanges();
+            }
+        }
+
+        public static Order GetOrder(string invoice)
+        {
+            return context.Orders.FirstOrDefault(e => e.InvoiceNumber == invoice);
+        }
+    }
+}
