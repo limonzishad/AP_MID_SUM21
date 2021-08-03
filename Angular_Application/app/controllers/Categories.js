ec25230f95099fdab3bb455b31baaf63b7abee40
Zishad Hossain Limon <zishadlimon@gmail.com>
2021-08-04 03:17:28 +0600

    controller updated!

--- Angular_Application/app/controllers/Categories.js
+++ Angular_Application/app/controllers/Categories.js
@@ -1,5 +1,5 @@
 app.controller("Categories",function($scope,$http,ajax){
-  ajax.get("https://localhost:44386/api/Category/GetAll",success,error);
+  ajax.get("https://localhost:44358/api/Category/GetAll",success,error);
   function success(response){
     $scope.Categories=response.data;
   }
--- Angular_Application/app/controllers/Products.js
+++ Angular_Application/app/controllers/Products.js
@@ -1,5 +1,5 @@
 app.controller("Products",function($scope,$http,ajax){
-  ajax.get("https://localhost:44386/api/Product/GetAll",success,error);
+  ajax.get("https://localhost:44358/api/Product/GetAll",success,error);
   function success(response){
     $scope.Products=response.data;
   }
